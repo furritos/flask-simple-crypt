460611b1efe589c513a39cb5ad9ef85a918ae0cc
Carlos Rivas <carlos@twobitcoder.com>
2016-11-25 15:59:33 -0800

    Bump version: 0.2.6 → 0.2.7

--- flask_simple_crypt.py
+++ flask_simple_crypt.py
@@ -21,7 +21,7 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Random.random import getrandbits
 from Crypto.Util import Counter
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 __author__ = "Carlos Rivas"
 __license__ = "BSD"
 __copyright__ = "(c) 2016 by Carlos Rivas\n" \
